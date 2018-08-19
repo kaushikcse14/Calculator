50d42e017b79f568ccd490c4e41151fb7f01eed6
ayush <kaushikcse14@gmail.com>
2018-08-19 11:25:50 +0530

    First commit

---
+++ WebContent/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+<h2>Calculator</h2>
+<form action="AddServlet" method="post">
+    <input type="number" name="num1"><b>+</b><input type="number" name="num2">    
+    <input type="submit" value="Calculate" />
+</form>
+</body>
+</html>
---
+++ src/com/nissan/training/AddServlet.java
@@ -0,0 +1,46 @@
+package com.nissan.training;
+
+import java.io.IOException;
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.nissan.training.AddNumber;
+
+/**
+ * Servlet implementation class AddServlet
+ */
+@WebServlet("/AddServlet")
+public class AddServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int n1, n2, res;
+		String num1=request.getParameter("num1");
+		String num2=request.getParameter("num2");
+		
+		n1 = Integer.parseInt(num1);
+		n2 = Integer.parseInt(num2);
+		AddNumber obj = new AddNumber();
+		res = obj.add(n1, n2);		
+		
+		PrintWriter writer = response.getWriter();
+		
+		writer.println("<h1>Hello " + res + "</h1>");
+		writer.close();
+	}
+}
